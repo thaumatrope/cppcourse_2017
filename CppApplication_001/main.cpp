b0a230ae632f74a8824d8aae4c0381ddb33addc0
jwest <jwest@192.168.0.129>
2017-09-13 12:16:49 +0200

    update

--- CppApplication_001/main.cpp
+++ CppApplication_001/main.cpp
@@ -12,6 +12,7 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
@@ -19,6 +20,8 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
+    
+    cout << "Hello World!\n";
 
     return 0;
 }
